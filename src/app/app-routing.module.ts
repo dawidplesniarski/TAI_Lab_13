9758c0920ca613d1c450f4d83bcc2028569ebdf5
Dawid Plesniarski <dawidplesniarski@gmail.com>
2020-03-27 16:43:31 +0100

    Lab complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { BlogComponent } from "./components/blog/blog.component";
 
 
 const routes: Routes = [
 {
-path:'',
+path:"",
 component: HomeComponent
 },
 {
-path:'contact',
-component: ContactComponent,
+path:"contact",
+component: ContactComponent
 },
 {
-path:'blog',
-component: BlogComponent,
-}];
+  path: "blog",
+  component: BlogComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
