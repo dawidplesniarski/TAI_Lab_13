0a743f392bb6533100b19af1f5aaee941db940f6
Dawid Plesniarski <dawidplesniarski@gmail.com>
2020-03-27 16:12:30 +0100

    Bootstrap and font awesome added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path:'',
+component: HomeComponent
+},
+{
+path:'contact',
+component: ContactComponent,
+},
+{
+path:'blog',
+component: BlogComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
