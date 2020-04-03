57e4ac03febd8b179516c84b1f832056117e985d
dawidplesniarski <dawidplesniarski@gmail.com>
2020-04-03 15:56:54 +0200

    summary pipe added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { BlogItemComponent } from './components/blog-item/blog-item.component';
-import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
-import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
-import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {BlogComponent} from './components/blog/blog.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {BlogItemComponent} from './components/blog-item/blog-item.component';
+import {BlogItemTextComponent} from './components/blog-item-text/blog-item-text.component';
+import {BlogItemImageComponent} from './components/blog-item-image/blog-item-image.component';
+import {BlogItemDetailsComponent} from './components/blog-item-details/blog-item-details.component';
 import {HttpClientModule} from '@angular/common/http';
 import {DataService} from './services/data.service';
+import {SummaryPipe} from './pipes/summary.pipe';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {DataService} from './services/data.service';
     BlogItemComponent,
     BlogItemTextComponent,
     BlogItemImageComponent,
-    BlogItemDetailsComponent
+    BlogItemDetailsComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
@@ -36,4 +38,5 @@ import {DataService} from './services/data.service';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
