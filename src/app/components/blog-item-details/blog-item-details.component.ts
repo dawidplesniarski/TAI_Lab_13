69d7f82b2c75cb234ad2878b14b65d4287a4b171
dawidplesniarski <dawidplesniarski@gmail.com>
2020-04-03 14:37:17 +0200

    components added

---
+++ src/app/components/blog-item-details/blog-item-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-item-details',
+  templateUrl: './blog-item-details.component.html',
+  styleUrls: ['./blog-item-details.component.scss']
+})
+export class BlogItemDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
