36c8202a7b3874cbde5b5da4571b1bb6618d9604
dawidplesniarski <dawidplesniarski@gmail.com>
2020-04-17 21:07:15 +0200

    Component Selectize added

---
+++ src/app/components/selectize/selectize.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectize',
+  templateUrl: './selectize.component.html',
+  styleUrls: ['./selectize.component.scss']
+})
+export class SelectizeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
