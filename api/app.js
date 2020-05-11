0d4a0e911a5ad4f21bca933128232b54e35ee22c
dawidplesniarski <dawidplesniarski@gmail.com>
2020-05-11 09:10:21 +0200

    Project structure and modules added

--- api/app.js
+++ api/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 import express from 'express';
+import routes from './REST/routes'
 
 const app = express();
 
 
 app.use(express.static('public'));
 
-
+routes(app);
 app.listen(process.env.PORT || 3000, function () {
   console.info('Server is running')
 });
