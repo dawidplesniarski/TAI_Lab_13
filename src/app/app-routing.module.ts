f795d7c1081c3183a5111cfb6e28c3fa954db2bf
dawidplesniarski <dawidplesniarski@gmail.com>
2020-05-22 12:26:37 +0200

    Login & signup components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { BlogComponent } from "./components/blog/blog.component";
 import {BlogItemDetailsComponent} from './components/blog-item-details/blog-item-details.component';
 import {BlogHomeComponent} from './components/blog-home/blog-home.component';
 import {AddPostComponent} from './components/add-post/add-post.component';
+import {AuthGuard} from './services/auth.guard';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
 
 
 const routes: Routes = [
@@ -14,12 +17,18 @@ path:"",
 component: HomeComponent
 },
 {
+path:'',
+  component: HomeComponent,
+  canActivate: [AuthGuard]
+},
+{
 path:"contact",
 component: ContactComponent
 },
 {
   path: "blog",
-  component: BlogHomeComponent
+  component: BlogHomeComponent,
+  canActivate: [AuthGuard]
 },
 {
   path: "blog/detail/:id",
@@ -28,7 +37,15 @@ component: ContactComponent
 {
   path:'add-post',
   component: AddPostComponent
-}
+},
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'signup',
+    component: SignupComponent
+  }
 ];
 
 @NgModule({
